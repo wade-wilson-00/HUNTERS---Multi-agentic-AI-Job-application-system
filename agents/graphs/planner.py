ee83f79fb51fdd3cd1e643d5837a2a103d01ccc9
wade-wilson-00 <caffeineaddict00dev@gmail.com>
2026-07-17 16:27:01 +0530

    feat: Initialized a Planner Node with access to MCP tools designed only for Hunter agent

--- agents/graphs/planner.py
+++ agents/graphs/planner.py
@@ -1,22 +1,7 @@
+from langchain_groq import ChatGroq
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from agents.graphs.hunter_state import HunterState
 from hunter_state import HunterState
 from groq_llm import chat_llm
-import sys
- 
+
 async def planner_node(state: HunterState):
-
-    client = MultiServerMCPClient({
-        "hunter_tools": {
-            "command": sys.executable,
-            "args": ["-m","mcp_server.central_server"],
-            "transport":"stdio",
-        }
-    })
-
-    tools = await client.get_tools()
-
-    llm_with_tools = chat_llm.bind_tools(tools)
-    response = await llm_with_tools.ainvoke(state["messages"])
-
-    return {"messages": [response]} 
+    
