009e01d3f51f1bec27e80974c46833829026cdb3
wade-wilson-00 <caffeineaddict00dev@gmail.com>
2026-08-15 16:09:09 +0530

    feat: Added additional state variables for agents to remember structured knowledge, instead of searching through whole conversations from the State.

--- agents/graphs/hunter_state.py
+++ agents/graphs/hunter_state.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated,Optional
 from langgraph.graph.message import add_messages
 from langchain_core.messages import AnyMessage
 
@@ -6,6 +6,13 @@ class HunterState(TypedDict):
     """The memory object passed from Node to Node in LangGraph."""
 
     messages:      Annotated[list[AnyMessage], add_messages]
+    summary: str
+
+    #Optional Fields for State variables
+    target_role: Optional[str]
+    preffered_role: Optional[str]
+    active_company: Optional[str]
+
     intent:        str
     final_response: str
     cached_resume:  str   # Resume text — loaded once, injected into every planner call
